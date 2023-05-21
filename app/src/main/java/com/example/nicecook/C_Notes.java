05e4074d7f06b3c44a98ca7f74fd597202ea4f70
fronald143 <120772118+fronald143@users.noreply.github.com>
2023-05-21 18:33:36 +0800

    notes

---
+++ app/src/main/java/com/example/nicecook/C_Notes.java
@@ -0,0 +1,19 @@
+package com.example.nicecook;
+
+public class C_Notes {
+    String notesName;
+    String description;
+    String time;
+
+    public String getNotesName() {
+        return notesName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ app/src/main/java/com/example/nicecook/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.example.nicecook;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+    Context context;
+    ArrayList<C_Notes> list;
+
+    public MyAdapter(Context context, ArrayList<C_Notes> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.notes_card,parent,false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        C_Notes notes = list.get(position);
+        holder.noteName.setText(notes.getNotesName());
+        holder.description.setText(notes.getDescription());
+        holder.time.setText(notes.getTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView noteName,description,time;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            noteName = itemView.findViewById(R.id.txtCaldereta);
+            description = itemView.findViewById(R.id.txtLabelOne);
+            time = itemView.findViewById(R.id.txtTime);
+        }
+    }
+}

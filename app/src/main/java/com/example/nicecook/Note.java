e60cda1c8c27e13f047d02ecf4bc9ecc1fb7b375
tr1b1iwnl <tr1b1iwnl@gmail.com>
2023-05-21 20:03:49 +0800

    Notes

---
+++ app/src/main/java/com/example/nicecook/Note.java
@@ -0,0 +1,19 @@
+package com.example.nicecook;
+
+public class Note {
+    String notesName;
+    String description;
+    String time;
+
+    public String getNotesName() {
+        return notesName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}

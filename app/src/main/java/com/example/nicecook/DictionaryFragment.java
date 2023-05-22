dfe5367cee10da8566d72ab0c0e3786d39727ee6
tr1b1iwnl <tr1b1iwnl@gmail.com>
2023-05-22 18:49:10 +0800

    Implemented Dictionary Page

---
+++ app/src/main/java/com/example/nicecook/DictionaryFragment.java
@@ -0,0 +1,126 @@
+package com.example.nicecook;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.appcompat.widget.SearchView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DictionaryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DictionaryFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private RecyclerView recyclerView;
+    private DatabaseReference databaseReference;
+    private DictionaryAdapter dictionaryAdapter;
+    private ArrayList<DictionaryEntry> list;
+    private SearchView searchViewSearch;
+
+    public DictionaryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DictionaryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DictionaryFragment newInstance(String param1, String param2) {
+        DictionaryFragment fragment = new DictionaryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
+        recyclerView = view.findViewById(R.id.wordList);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Dictionary");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        list = new ArrayList<>();
+        dictionaryAdapter = new DictionaryAdapter(getContext(), list);
+        recyclerView.setAdapter(dictionaryAdapter);
+        searchViewSearch = view.findViewById(R.id.searchViewSearch);
+        searchWord("");
+
+        searchViewSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchWord(s);
+                return true;
+            }
+        });
+
+        return view;
+    }
+
+    private void searchWord(String word) {
+        databaseReference.orderByChild("word").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    DictionaryEntry dictionaryEntry = dataSnapshot.getValue(DictionaryEntry.class);
+                    if(dictionaryEntry != null && dictionaryEntry.getWord().toLowerCase().contains(word.toLowerCase())) {
+                        list.add(dictionaryEntry);
+                    }
+                }
+                dictionaryAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
